6537f21c86e26b4ea8b19cb3b1c74f4d86a42914
Dario Balboni <dario.balboni.96@gmail.com>
2022-02-12 18:07:55 +0100

    Very basic chord matching and reporting

--- main.ts
+++ main.ts
@@ -1,137 +1,192 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Prec } from '@codemirror/state';
+import { EditorView } from '@codemirror/view';
 
-// Remember to rename these classes and interfaces!
+class HotKey {
+    public meta: boolean;
+    public shift: boolean;
+    public ctrl: boolean;
+    public alt: boolean;
+    public key: string;
 
-interface MyPluginSettings {
-	mySetting: string;
+    public constructor (dict) {
+	this.meta = dict.meta;
+	this.shift = dict.shift;
+	this.ctrl = dict.ctrl;
+	this.alt = dict.alt;
+	this.key = dict.key.toLowerCase();
+    }
+    
+    public toString(): string {
+	var name = "";
+	if (this.alt == true) {
+	    name += "A-";
+	}
+	if (this.ctrl == true) {
+	    name += "C-";
+	}
+	if (this.meta == true) {
+	    name += "M-";
+	}
+	if (this.shift == true) {
+	    name += "S-";
+	}
+	return name + this.key;
+    }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+class Chord {
+    public sequence: HotKey[];
+    public command: string;
+
+    public constructor (dict) {
+	this.sequence = dict.sequence;
+	this.command = dict.command;
+    }
+    
+    // Check if other is a prefix of the current Chord
+    // Returns: "NO", "YES", "FULL" when it is a complete match
+    public checkPrefix(other: HotKey[]): string {
+	for (const [index, hotkey] of this.sequence.entries()) {
+	    let otherkey = other[index];
+	    if (otherkey === undefined) {
+		return "YES";
+	    }
+	    if ((hotkey.meta != otherkey.meta) ||
+		(hotkey.shift != otherkey.shift) ||
+		(hotkey.ctrl != otherkey.ctrl) ||
+		(hotkey.alt != otherkey.alt) ||
+		(hotkey.key != otherkey.key)) {
+		return "NO";
+	    }
+	}
+	return "FULL";
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+interface Settings {
+    hotkeys: Chord[];
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+const DEFAULT_SETTINGS: Settings = {
+    hotkeys: [
+	new Chord({ sequence: [
+	    new HotKey({ key: 'h', meta: false, shift: false, ctrl: false, alt: true }),
+	], command: "editor:focus-left" }),
+	new Chord({ sequence: [
+	    new HotKey({ key: 'x', meta: false, shift: false, ctrl: true, alt: false }),
+	    new HotKey({ key: 'h', meta: false, shift: false, ctrl: false, alt: false }),
+	], command: "command-palette:open" }),
+    ]
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export default class HotkeysChordPlugin extends Plugin {
+    public settings: Settings;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+    private statusbar; // Points to our custom status bar
+    private currentseq: HotKey[]; // List of currently pressed chords
+
+    async onload() {
+	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	this.statusbar = this.addStatusBarItem();
+	this.currentseq = [];
+
+	this.updateStatusBar();
+	this.registerEditorExtension(
+	    Prec.highest(
+		EditorView.domEventHandlers({
+		    "keydown": this.handleKeyDown,
+		})
+	    )
+	);
+
+	// TODO: Add the settings tab!
+	// this.addSettingsTab is not a function!!
+	// this.addSettingsTab(new HotkeysChordPluginSettingsTab(this.app, this));
+    }
+
+    async saveSettings() {
+	await this.saveData(this.settings);
+    }
+
+    private updateStatusBar(): void {
+	var chord = "None";
+	if (this.currentseq.length > 0) {
+	    chord = this.currentseq.map(hk => hk.toString()).join(" ");
 	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	this.statusbar.setText("Chord: " + chord);
+    }
+    
+    private readonly handleKeyDown = (
+	event: KeyboardEvent,
+	cm: CodeMirror.Editor,
+    ) => {
+	if (event.key === 'Shift' || event.key === 'Meta' || event.key === 'Control' || event.key == 'Alt') {
+	    console.debug("Skipping meta key: " + event.key);
+	    return;
 	}
+	// Add the pressed keys to the current sequence and update on-screen
+	let hotkey = new HotKey ({
+	    key: event.key,
+	    shift: event.shiftKey,
+	    meta: event.metaKey,
+	    ctrl: event.ctrlKey,
+	    alt: event.altKey,
+	});
+	this.currentseq.push(hotkey);
+	// We check whether the current sequence can be found in the hotkey database
+	var partialMatch = false;
+	for (let chord of this.settings.hotkeys) {
+	    let result = chord.checkPrefix(this.currentseq);
+	    if (result == "FULL") {
+		(this.app as any).commands.executeCommandById(chord.command);
+		event.preventDefault();
+		event.stopPropagation();
+		// TODO: Maybe put this as an option
+		new Notice(`Chord triggered ${chord.command}`);
+		partialMatch = false;
+		break;
+	    } else if (result == "YES") {
+		partialMatch = true;
+	    }
+	}
+	// We also want to prevent default if this is a key inside a previous sequence or a partial match
+	if ((this.currentseq.length > 1) || partialMatch) {
+	    event.preventDefault();
+	    event.stopPropagation();
+	}
+	if (!partialMatch) {
+	    // No patial match, we get back to zero sequence
+	    this.currentseq = [];
+	}
+	this.updateStatusBar();
+    }
+}
+
+class HotkeysChordPluginSettingsTab extends PluginSettingTab {
+    plugin: HotkeysChordPlugin;
+
+    constructor(app: App, plugin: HotkeysChordPlugin) {
+	super(app, plugin);
+	this.plugin = plugin;
+    }
+    
+    public display(): void {
+	const {containerEl} = this;
+	containerEl.empty();
+	containerEl.createEl('h2', {text: 'Hotkeys Chord Plugin - Settings'});
+
+	// TODO: Show currently defined hotkeys and allow to add others!
+	new Setting(containerEl)
+	    .setName('Setting #1')
+	    .setDesc('It\'s a secret')
+	    .addText(text => text
+	    .setPlaceholder('Enter your secret')
+	    .setValue(this.plugin.settings.mySetting)
+	    .onChange(async (value) => {
+		console.log('Secret: ' + value);
+		this.plugin.settings.mySetting = value;
+		await this.plugin.saveSettings();
+	    }));
+    }
 }
